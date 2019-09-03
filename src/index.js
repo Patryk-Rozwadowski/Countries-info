a3c848b12a1e350f3c4d0514d482c40f2f7fed88
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-09-03 02:50:52 +0200

    Fixed bootstrap

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store/index';
 import DevTools from './DevTools/devTools';
 import { getCountries } from './actions/actionCreator';
+import Navigation from './presentational/navigation.component';
 
 render(
     <Provider store={store}>
-        <div>
-            <h1>Inicjalizacja projektu</h1>
+        <Router history={hashHistory} >
+            <Route path='/' component={Navigation} />
+            < h1 > Inicjalizacja projektu</h1 >
             <DevTools />
-        </div>
+        </Router>
     </Provider>,
     document.getElementById('root')
 );
