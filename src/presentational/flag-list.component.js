1007dc48911fda38d73ba4cc538e163769ac934b
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-09-20 23:46:06 +0200

    Add grid for countries

--- src/presentational/flag-list.component.js
+++ src/presentational/flag-list.component.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Link } from 'react-router';
 import CountryFlag from '../presentational/flag.component';
 
+import '../country.css';
+
 const CountryFlagList = (props) => (
-    <div className="countries-list">
+    <div className="countries-list mt-5 text-center align-items-center">
         {props.countries.map(country => {
             return (
-                <div className="single-country" key={country.id}>
+                <div className='col-6' key={country.id}>
                     <Link className='logo' to={'countries/country/' + country.id}>
                         <CountryFlag country={country} />
                     </Link>
