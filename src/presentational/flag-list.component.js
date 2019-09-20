cd44960b6f0d7b94e8d9a7392c303e00a5afda41
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-09-20 23:48:27 +0200

    Add styles for button

--- src/presentational/flag-list.component.js
+++ src/presentational/flag-list.component.js
@@ -8,11 +8,11 @@ const CountryFlagList = (props) => (
     <div className="countries-list mt-5 text-center align-items-center">
         {props.countries.map(country => {
             return (
-                <div className='col-6' key={country.id}>
+                <div key={country.id}>
                     <Link className='logo' to={'countries/country/' + country.id}>
                         <CountryFlag country={country} />
                     </Link>
-                    <button onClick={props.deleteCountry.bind(null, country.id)}>DELETE</button>
+                    <button className='btn btn-danger mt-2' onClick={props.deleteCountry.bind(null, country.id)}>DELETE</button>
                 </div>
             )
         })}
