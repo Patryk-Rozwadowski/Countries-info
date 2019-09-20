03cb1eafc56b57ac49e5b8c226091f4d7506981d
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-09-20 22:50:24 +0200

    Add nav styles

--- src/presentational/navigation.component.js
+++ src/presentational/navigation.component.js
@@ -3,16 +3,26 @@ import { Link } from 'react-router';
 import 'bootstrap/dist/css/bootstrap.css';
 const Navigation = props => (
     <div>
-        <nav className="navbar navbar-default">
+        <nav className="navbar navbar-dark bg-dark">
             <div className="container-fluid">
                 <div className="navbar-header">
                     <Link className="navbar-brand" to="/">Państwa.js</Link>
                 </div>
-                <div className="navbar-collapse">
-                    <ul className="nav navbar-nav">
-                        <li><Link to="/countries">Countries</Link></li>
-                        <li><Link to="/continents">Continents</Link></li>
-                        <li><Link to="/contact">Contact</Link></li>
+                <div className="navbar-collapse row">
+                    <ul className="nav justify-content-center">
+
+                        <li className='nav-link'>
+                            <Link className='text-light' to="/countries">Countries</Link>
+                        </li>
+
+                        <li className='nav-link'>
+                            <Link className='text-light' to="/continents">Continents</Link>
+                        </li>
+
+                        <li className='nav-link'>
+                            <Link className='text-light' to="/contact">Contact</Link>
+                        </li>
+                        
                     </ul>
                 </div>
             </div>
